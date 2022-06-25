1a9d1d456442dc1e4d86218dfb7f1d33f5232a08
Oshanimadhushika <oshanimadhushika12@gmail.com>
2022-06-25 21:57:18 +0530

    Add Annotation to Room

--- src/lk/ijse/hybernate/entity/Room.java
+++ src/lk/ijse/hybernate/entity/Room.java
@@ -1,9 +1,15 @@
 package lk.ijse.hybernate.entity;
 
 import lombok.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,10 +19,16 @@ import javax.persistence.Id;
 @Entity(name = "room")
 public class Room {
     @Id
-    private String roomId;
-    private String roomType;
-    private String keyMoney;
-    private int roomQty;
+    private String room_type_id;
+    @Column(nullable = false)
+    private String type;
+    @Column(nullable = false)
+    private String key_money;
+    @Column(nullable = false)
+    private int qty;
 
 
+    @OneToMany(mappedBy = "Room")
+    @Cascade(CascadeType.ALL)
+   List<Student> studentList=new ArrayList<>();
 }
