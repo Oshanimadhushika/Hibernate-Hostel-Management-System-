2a72b71aade8130dbee9909f950c338e7ac3e48f
Oshanimadhushika <oshanimadhushika12@gmail.com>
2022-06-25 22:31:13 +0530

    Added UserLogin Entity

---
+++ src/lk/ijse/hybernate/entity/UserLogin.java
@@ -0,0 +1,23 @@
+package lk.ijse.hybernate.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "userlogin")
+public class UserLogin {
+    @Id
+    private String user_id;
+    @Column(nullable = false)
+    private String user_name;
+    @Column(nullable = false)
+    private String passward;
+}
